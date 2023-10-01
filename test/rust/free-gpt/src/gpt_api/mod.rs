b6c069db13cb38b2cbd3545a5eec2e3ee7faac7b
drakos74 <drakos74@gmail.com>
2023-10-01 11:48:41 +0200

    fix proper conversation history

--- test/rust/free-gpt/src/gpt_api/mod.rs
+++ test/rust/free-gpt/src/gpt_api/mod.rs
@@ -1,22 +1,49 @@
 pub mod text {
 
+    use crate::db::user::History;
     use async_openai::{
-        types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+        types::{
+            ChatCompletionRequestMessage, ChatCompletionRequestMessageArgs,
+            CreateChatCompletionRequestArgs, Role,
+        },
         Client,
     };
     use futures::StreamExt;
     use std::error::Error;
 
-    pub async fn call(request: &str) -> Result<String, Box<dyn Error>> {
+    pub async fn chat(request: &str, history: &Vec<History>) -> Result<String, Box<dyn Error>> {
         let client = Client::new();
+
+        let mut messages: Vec<ChatCompletionRequestMessage> = Vec::new();
+        for instance in history {
+            messages.push(
+                ChatCompletionRequestMessageArgs::default()
+                    .content(instance.message.clone())
+                    .role(Role::User)
+                    .build()?,
+            );
+            messages.push(
+                ChatCompletionRequestMessageArgs::default()
+                    .content(instance.response.clone())
+                    .role(Role::Assistant)
+                    .build()?,
+            );
+        }
 
         let request = CreateChatCompletionRequestArgs::default()
             .model("gpt-3.5-turbo")
             .max_tokens(1024u16)
-            .messages([ChatCompletionRequestMessageArgs::default()
-                .content(request)
-                .role(Role::User)
-                .build()?])
+            .messages(
+                [
+                    messages.clone(),
+                    [ChatCompletionRequestMessageArgs::default()
+                        .content(request)
+                        .role(Role::User)
+                        .build()?]
+                    .to_vec(),
+                ]
+                .concat(),
+            )
             .build()?;
 
         let mut stream = client.chat().create_stream(request).await?;
--- test/rust/free-gpt/src/main.rs
+++ test/rust/free-gpt/src/main.rs
@@ -1,10 +1,16 @@
 #[macro_use]
 extern crate rocket;
 
+mod db;
 mod gpt_api;
 mod prompts;
 
-use crate::gpt_api::text::call;
+use crate::db::user::get;
+use crate::db::user::put;
+
+use crate::db::user::History;
+
+use crate::gpt_api::text::chat;
 use crate::prompts::user::greet;
 
 use std::io::{stdout, Write};
@@ -14,8 +20,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct Request {
-    name: String,
     id: u8,
+    title: String,
     input: String,
 }
 
@@ -26,17 +32,27 @@ fn rocket() -> _ {
 
 #[get("/free-gpt/<user>/<request>")]
 async fn get_gpt(user: &str, request: &str) -> String {
-    let respone = call(request);
+    let history = Vec::new();
+    let respone = chat(request, &history);
     return respone.await.unwrap_or("error".to_string());
 }
 
 #[post("/free-gpt/v1/<user>", format = "json", data = "<request>")]
 async fn post_gpt(user: &str, request: Json<Request>) -> String {
+    // first get the users conversation
+    let conversation = get(user, request.title.to_string(), request.input.to_string());
+    // read the content from the request
     let content = request.input.as_str();
-
+    // apply prompts ...
     let message = greet(user, content);
-
-    let response = call(&message);
-
-    return response.await.unwrap_or("error".to_string());
+    // send request ...
+    let response = chat(&message, &conversation.messages);
+    let response_message = response.await.unwrap_or("error".to_string());
+    let response_history = History {
+        message: request.input.to_string(),
+        response: response_message.clone(),
+    };
+    put(user, conversation, response_history);
+    // open response ...
+    return response_message.clone();
 }
